b9abe994e0d25efd8020db7dcf5d135440aacf6a
Swanny <aswanson007@gmail.com>
2021-08-18 13:49:45 -0700

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+//function App() {  -> replaced with below arrow function syntax which provides same results
+const App = () => {
+  return (
+    <div>
+      <h1>Images Gallery</h1>
+    </div>
+  );
+}
+
+export default App;
