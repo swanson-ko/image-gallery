6e3c0a7820e28a0d4e048ca9e83ba9d87cc7d06e
Swanny <aswanson007@gmail.com>
2021-08-20 14:24:22 -0700

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 //function App() {  -> replaced with below arrow function syntax which provides same results
 const App = () => {
   return (
     <div>
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
