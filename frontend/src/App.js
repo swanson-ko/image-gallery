c28fc4c97257de12491a8403ef848e19c330c2e3
Swanny <aswanson007@gmail.com>
2021-08-24 15:40:43 -0700

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
 // ways to grab all updated values in the search component
-import { useState } from 'react'; 
+import { useState } from 'react';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -15,26 +14,28 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      } )
+      });
     setWord('');
-  }
+  };
 
-    // invoke event log and display search term as being entered by the user...
-    //console.log(process.env.REACT_APP_UNSPLASH_KEY);
+  // invoke event log and display search term as being entered by the user...
+  //console.log(process.env.REACT_APP_UNSPLASH_KEY);
 
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
