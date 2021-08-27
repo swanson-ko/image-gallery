1bb2a1810e750f5e0cb8c426e7b6c0122904e82f
Swanny <aswanson007@gmail.com>
2021-08-27 12:48:29 -0700

    added svg logo to header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
   backgroundColor: 'lightblue',
@@ -8,7 +9,7 @@ const Header = (props) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '4rem' }} />
       </Container>
     </Navbar>
   );
